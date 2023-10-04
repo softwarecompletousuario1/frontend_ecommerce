cda390f3cbed76f15f09b8a490b2aa130c2c6011
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-10-04 15:29:41 -0500

    Update

--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,3 +1,1 @@
-export { Header } from "./Header"
-export { Footer } from "./Footer"
 export { Layout } from "./Layout"
--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -5,9 +5,9 @@ import type { ManifestOptions } from "vite-plugin-pwa"
  * Defines the default SEO configuration for the website.
  */
 export const seoConfig = {
-	baseURL: "https://example.com", // Change this to your production URL.
+	baseURL: "https://softwarecompleto.com", // Change this to your production URL.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
+		"softwarecompleto.com es una plataforma de servicios de software, enfocada en la unificación de ecommerce y software contable para microempresas", // Change this to be your website's description.
 	type: "website",
 	image: {
 		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
@@ -15,7 +15,7 @@ export const seoConfig = {
 		width: 1200,
 		height: 630
 	},
-	siteName: "Astro PWA Starter", // Change this to your website's name,
+	siteName: "softwarecompleto.com", // Change this to your website's name,
 	twitter: {
 		card: "summary_large_image"
 	}
@@ -25,10 +25,10 @@ export const seoConfig = {
  * Defines the configuration for PWA webmanifest.
  */
 export const manifest: Partial<ManifestOptions> = {
-	name: "Astro PWA Starter", // Change this to your website's name.
-	short_name: "Astro PWA Starter", // Change this to your website's short name.
+	name: "softwarecompleto.com", // Change this to your website's name.
+	short_name: "softwarecompleto.com", // Change this to your website's short name.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to your websites description.
+		"softwarecompleto.com es una plataforma de servicios de software, enfocada en la unificación de ecommerce y software contable para microempresas", // Change this to your websites description.
 	theme_color: "#30E130", // Change this to your primary color.
 	background_color: "#ffffff", // Change this to your background color.
 	display: "minimal-ui",
