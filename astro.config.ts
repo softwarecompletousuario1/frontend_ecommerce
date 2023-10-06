7d42df400b5307a1d5e71428339c6dacfaedd88e
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-10-06 09:39:13 -0500

    Update

--- astro.config.ts
+++ astro.config.ts
@@ -1,45 +1,44 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
 // Astro integration imports
-import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap"
-import compress from "astro-compress"
-import { VitePWA } from "vite-plugin-pwa"
-import image from '@astrojs/image'
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import { VitePWA } from "vite-plugin-pwa";
+
+import image from '@astrojs/image';
 
 // Helper imports
-import { manifest, seoConfig } from "./utils/seoConfig"
+import { manifest, seoConfig } from "./utils/seoConfig";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-	site: seoConfig.baseURL,
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-				path: "./tailwind.config.js"
-			}
-		}),
-		sitemap(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		  }),
-		compress()
-	],
-	vite: {
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				workbox: {
-				  globDirectory: 'dist',
-				  globPatterns: [
-				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-				  ],
-				  // Don't fallback on document based (e.g. `/some-page`) requests
-				  // This removes an errant console.log message from showing up.
-				  navigateFallback: null,
-				},
-			})
-		]
-	}
-})
+  site: seoConfig.baseURL,
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false,
+      path: "./tailwind.config.js"
+    }
+  }), sitemap(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), compress(),
+  react({
+    include: ['**/react/*'],
+  }),
+
+],
+  vite: {
+    plugins: [VitePWA({
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        globDirectory: 'dist',
+        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null
+      }
+    })]
+  }
+});
---
+++ src/components/global/home/home_slider_1_carrusel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Carrusel = () => {
+  
+  return (
+    <div>
+      soy carrusel
+    </div>
+  );
+};
+
+export default Carrusel;
