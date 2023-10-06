f46f8b2b474ee1e6df7160da356d3d390f54f277
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-10-06 11:17:20 -0500

    Update

--- src/components/global/home/home_slider_1_carrusel.jsx
+++ src/components/global/home/home_slider_1_carrusel.jsx
@@ -1,19 +1,20 @@
 import React, {useState} from 'react';
-import Styles from '../../../styles/pages/home/home_slider_1_carrusel.css'
-
+// import Styles from '../../../styles/pages/home/home_slider_1_carrusel.css';
+// import { Image } from '@astrojs/image/components';
 
 const Carrusel = () => {
   const imagenes_carrusel = [
-    'https://http2.mlstatic.com/D_NQ_829080-MLA72001750577_092023-OO.webp',
+    'https://res.cloudinary.com/unidigital/image/upload/v1696605197/software%20completo/promo_isfjtt.png',
     'https://http2.mlstatic.com/D_NQ_885299-MLA71946536612_092023-OO.webp',
     'https://http2.mlstatic.com/D_NQ_827915-MLA72037073590_102023-OO.webp',
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(2);
-  console.log(imagenes_carrusel.length)
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
   const imagenActual = imagenes_carrusel[currentIndex];
 
   const anteriorFuncion = () =>{
+    alert("botonAntetior");
     if (currentIndex <= 0){
       setCurrentIndex(0);
     }else{
@@ -29,13 +30,14 @@ const Carrusel = () => {
     }
   }
 
+  console.log(imagenActual);
   return (
-    <div className={Styles.content_slider_1_carrusel}>
+    <div >
       <div>
-      <img src={imagenActual} alt="Imagen del carrusel" />
-      <button onClick={anteriorFuncion}>Anterior</button>
-      <button onClick={siguienteFuncion}>Siguiente</button>
-    </div>
+        <img src={imagenActual} alt="Imagen del carrusel" />
+        <button onClick={anteriorFuncion}>Anterior</button>
+        <button onClick={siguienteFuncion}>Siguiente</button>
+      </div>
     </div>
   );
 };
